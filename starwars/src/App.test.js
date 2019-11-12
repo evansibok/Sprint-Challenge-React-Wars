60582db6df793cd102b947d325f8a1f5dc5c8bd8
Evans Ibok <evansibok@gmail.com>
2019-11-12 16:36:31 +0100

    completed a test that verifies that the App component renders

--- starwars/src/App.test.js
+++ starwars/src/App.test.js
@@ -3,6 +3,14 @@ import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-it("renders without crashing", () => {
+afterEach(rtl.cleanup);
 
+let wrapper;
+
+beforeEach(() => {
+  wrapper = rtl.render(<App />)
+})
+
+it("renders without crashing", () => {
+  expect(wrapper.queryByText(/react wars/i)).toBeInTheDocument();
 })
